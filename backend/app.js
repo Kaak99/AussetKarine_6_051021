27377c453b3ff04712870e44e3d4a2eba675565a
Kaak <cours.kaak@gmail.com>
2021-11-03 16:28:37 +0100

    bdd connectée

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,28 @@ const express = require('express');
 
 const app = express();
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://master:master-mdp@cluster0.uyax9.mongodb.net/maBdd?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
+});
+
+app.use(express.json());
+
+app.post('/api/stuff', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Objet créé !'
+  });
 });
 
 
