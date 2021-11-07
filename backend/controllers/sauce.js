305d669973a74581f69218fffcfeed8e05d504f2
Kaak <cours.kaak@gmail.com>
2021-11-07 18:57:21 +0100

    efface mais sauce undefined

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -36,7 +36,7 @@ exports.modifySauce = (req, res, next) => {
 exports.deleteSauce = (req, res, next) => {
 
   Sauce.deleteOne({ _id: req.params.id })
-    .then(sauce => res.status(200).json({ message: 'Objet deleted !'}).send(sauce))
+    .then(sauce => res.status(200).json(sauce))
     .catch(error => res.status(400).json({ error }));
 };
 
