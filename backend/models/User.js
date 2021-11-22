426ee04d3f3a277bfaf8384164c17927950e2aa3
Kaak <cours.kaak@gmail.com>
2021-11-22 21:36:32 +0100

    conditions sur modele user

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  email: { type: String, required: true, unique: true, lowercase: true, trim: true},
+  password: { type: String, required: true, minLength:6, maxLength:1024 }
 });
 
 userSchema.plugin(uniqueValidator);
