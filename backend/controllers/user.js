3adfd9d428b8f77412bc56e37f35fc39a916194d
Kaak <cours.kaak@gmail.com>
2021-11-29 12:55:32 +0100

    creation user avec email crypté et mdp securisé ok
    front8

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -5,25 +5,25 @@ console.log(` --------> user-ctrl`);
 
 const User = require('../models/User');//importe le modele
 const bcrypt = require('bcrypt');//(hash mdp)
-//const cryptojs = require("crypto-js");//(chiffrage pour emails)
+const cryptojs = require('crypto-js');//(chiffrage pour emails)
 const jwt = require('jsonwebtoken');
-//const dotenv = require("dotenv").config();//import variables d'environnement
+const dotenv = require("dotenv").config();//import variables d'environnement
 
 
 //-----exports-----//
 
 
-//1.signup : on va enregistrer l'utilisateur dans la bdd
+//! 1.signup : on va enregistrer l'utilisateur dans la bdd
 exports.signup = (req, res, next) => {
-    //chiffrage email
-  //const emailCryptoJs = cryptojs.HmacSHA256(req.body.email, `${process.env.CRYPTOJS_EMAIL}`).toString();//si chiffrage mail!
+  // ! chiffrage email ! //
+  const emailCryptoJs = cryptojs.HmacSHA256(req.body.email, process.env.CRYPTOJS_EMAIL).toString();//si chiffrage mail!
   //console.log(process.env.CRYPTOJS_EMAIL);
-    //hashage mdp //
+  // ! hashage mdp ! //
   bcrypt.hash(req.body.password, 10)
     .then(hash => {
       const user = new User({
-        //email: emailCryptoJs,//si chiffrage mail!
-        email: req.body.email,
+        email: emailCryptoJs,//si chiffrage mail!
+        //email: req.body.email,//si pas de chiffrage mail!
         password: hash
       });
       user.save()
@@ -33,25 +33,25 @@ exports.signup = (req, res, next) => {
     .catch(error => res.status(500).json({ error }));
 };
 
-//2.login :
+//! 2.login :
 exports.login = (req, res, next) => {
-    //chiffrage email//
-  //const emailCryptoJs = cryptojs.HmacSHA256(req.body.email, `${process.env.CRYPTOJS_EMAIL}`).toString();//si chiffrage mail!
+  // ! chiffrage email ! //
+  const emailCryptoJs = cryptojs.HmacSHA256(req.body.email, process.env.CRYPTOJS_EMAIL).toString();//si chiffrage mail!
   //console.log(emailCryptoJs);
-    //chercher si email existe dans bdd //
-  //User.findOne({email:emailCryptoJs})//si chiffrage mail!
-  User.findOne({ email: req.body.email })
+  // ! chercher si email existe dans bdd ! //
+  User.findOne({email:emailCryptoJs})//si chiffrage mail!
+  //User.findOne({ email: req.body.email })//si pas de chiffrage mail!
     .then(user => {
       if (!user) {//si pas trouvé
         return res.status(401).json({ error: 'Utilisateur non trouvé !' });
       }
-      // si trouvé //
+      // ! si trouvé //
       bcrypt.compare(req.body.password, user.password)
         .then(valid => {
           if (!valid) {//si mdp pas valide
             return res.status(401).json({ error: 'Mot de passe incorrect !' });
           }
-          //si mdp valide //
+          // ! si mdp valide //
           console.log("welcome back user "+user._id+" !");
           res.status(200).json({
             userId: user._id,
@@ -59,9 +59,9 @@ exports.login = (req, res, next) => {
               { userId: user._id },
               process.env.JWT_SECRET_TOKEN,
               { expiresIn: '24h' }
-              //token contenant userId+expiration est envoyé au front lors login (avec useirId ; c'est le "payload")
+              //token contenant userId+expiration est envoyé au front lors login (= le "payload")
               //token contiendra avec le payload(=info à transmettre) un header et une signature
-              // (signature=issue de , au moyen de clé privé JWT_SECRET_TOKEN )
+              // (signature=issue de [header+payload], au moyen de clé privé JWT_SECRET_TOKEN )
               //ainsi toute modif de [header+payload] va changer la signature
               //(token controle 1/ l'authentification (remplace login/mdp) 2/son intégrité)
             )
